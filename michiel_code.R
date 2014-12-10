44364c6f75fe2eb244ab55c22aaf1f65442b014e
tom13878 <tomas.morley@wur.nl>
2014-12-10 17:09:36 +0100

    New file containing pieces of code in orginal year 2 preperation code which may need tpo be kept

---
+++ michiel_code.R
@@ -0,0 +1,53 @@
+# IPOP 2271000300 IPOP 3 (new code in 2014-228250008)
+# R code to prepare LSMS-ISA data for yield and adoption analysis
+
+# TO DO
+# Add soil and rain stress variables
+# add rain stress variable from community survey
+# Check for other control factors (see Burke)
+# Link CAN use to share of maize on plot. Suspician that for these plots Maize might not be key crop.
+# Distinguish between buyers and seller of Maize (see Sheahan)
+# Focus on two Maize regions (Southern Highlands and around Arusha) and compare results.
+# Add interaction N and irrigation (see Jayne and Rashid, p. 552)
+
+# VERSION CONTROL
+#version <-Sys.Date() # set to today's date
+#plots<-paste("Analysis/",version, "Graph", sep=" ") # create plot folder
+#dir.create(plots)
+#if (!file.exists(plots)) {
+#  dir.create(plots)
+#}
+
+# Load relevant community survey data
+filepath<-paste(wdpath, "\\Data\\Tanzania\\2010_11\\Stata\\TZNPS2COMDTA\\", sep="")
+y2commlink<-read.dta("./Data/Tanzania/2010_11/Stata/TZNPS2COMDTA/y2commlink.dta" ,convert.factors = TRUE) # in order to ensure that id_01 is numeric
+Com<-read.dta(paste(filepath,"y2commlink.dta", sep=""),convert.factors = TRUE)
+Com<-Com[,c(1,2)]
+names(Com)<-c("y2_hhid","Regions")
+
+Com<-merge(Com, y2commlink, by="y2_hhid") # note that not for all EAs community data is available - see overview document
+Com<-Com[complete.cases(Com),] # For about 600 plots the geodata is NA
+Com<-merge(Com,HH.Geovariables_Y2,by="y2_hhid")
+Com$District_id<-factor(with(Com, paste(Regions, id_02, sep="_")))
+EAs<-unique(na.omit(Com[,c(2:9)])) # list of all EAs plus geocoordinates, district and community ids.
+
+filepath<-paste(wdpath, "\\Data\\Tanzania\\2010_11\\Stata\\TZNPS2HH2DTA\\", sep="")
+HH_SEC_N<-read.dta(paste(filepath,"HH_SEC_N.dta", sep=""),convert.factors = FALSE)
+# Number of agricultural assets defined as: (1) small tools: 428, 429, 431, 435, 440, 441, 442, 443, 446, 447, 452; (2) 444, 445, 449 (see p. 40 HH Quest.)
+HHAssets<-HH_SEC_N[complete.cases(HH_SEC_N),]
+HHAssets$Tools<-recode(HHAssets$itemcode ,"c(428, 429, 431, 435, 440, 441, 442, 443, 446, 447, 452)='Small'; c(444, 445, 449)='Large'; else=NA")
+HHAssets<-dcast(HHAssets, y2_hhid~Tools, sum, value.var="hh_n01_2")
+
+# Create fertilizer variables for plot database
+Fert<-merge(Fert, Plot.Geovariables_Y2[,c(1,3,4)], by=c("y2_hhid", "plotnum"), all.x=TRUE)
+Fert2<-Fert %>%
+  group_by(y2_hhid, plotnum) %>%      
+  select(y2_hhid, plotnum, N, P, K) %>%
+  summarise(N=plus(N), P=plus(P), K=plus(K)) %>%
+  arrange(desc(N))
+Fert2[is.na(Fert2)]<-0
+Plot2<-merge(Plot2, Fert2, by=c("y2_hhid", "plotnum"))
+#InorgFertkg<-rowSums(cbind(AG_SEC3A$ag3a_47,AG_SEC3A$ag3a_54), na.rm=TRUE)
+#InorgFertkg<-cbind(AG_SEC3A[,c(1,2)], InorgFertkg)
+
+Plot2<-merge(Plot2, areas_tza_y2_imputed, by=c("y2_hhid", "plotnum"), all.x=TRUE)

b28860f823c47fbe19e70ccd34476e4e6c3d1398
tom13878 <tomas.morley@wur.nl>
2014-12-15 16:09:47 +0100

    code to create preipitation variables for 2008 and 2010 and write them to csv files

--- read_precip.R
+++ read_precip.R
@@ -1,64 +1,89 @@
+# ``````````````````````````````````````````````````````````````````````````````````````````````````
+# ``````````````````````````````````````````````````````````````````````````````````````````````````
+# Code for reading in nc files containing precipitation values. These values are then extracted for
+# the rainy season in 2008 and 2010
+# ``````````````````````````````````````````````````````````````````````````````````````````````````
+# ``````````````````````````````````````````````````````````````````````````````````````````````````
 setwd("C:/Users/morle001/Dropbox/Micro_IPOP")
 # 1. get the ncdf and raster packages from library and read the nc data directly as a
-#    raster object. In this
-#    case we read in a list 
+#    raster object. In this case we read in a list 
 library(ncdf)
 library(raster)
+library(dplyr)
 
-ncin_raster_y1 <- stack("./Data/Spatial/Climate_data/cru_ts3.22.2001.2010.pre.dat.nc")
-ncin_raster_y2 <- stack(" ./Data/Spatial/Climate_data/cru_ts3.22.2011.2013.pre.dat.nc")
-nlayers(ncin_raster_y1); nlayers(ncin_raster_y2)
-nrow(ncin_raster_y1); nrow(ncin_raster_y2)
-ncol(ncin_raster_y1); ncol(ncin_raster_y2)
+# 2.read in the precipitation data and the geo.vars data for 2008 and 2010
+precip.raster <- stack("./Data/Spatial/Climate_data/cru_ts3.22.2001.2010.pre.dat.nc")
 
-# 2. raster objects can be cropped usinga spatial object, in this case the spatial
-#    object is a SpatialPolygonsDataFrame making it possible to cut Tanzania 
-#    from the raster.
-TZA_map <- getData('GADM', country = "TZA", level = 1)
-raster_TZA_y1 <- crop(ncin_raster_y1, TZA_map)
-raster_TZA_y2 <- crop(ncin_raster_y2, TZA_map)
-str(raster_TZA_y1); str(raster_TZA_y2)
+# 3. cut out tanzania from raster object
+TZA.map <- getData('GADM', country = "TZA", level = 1)
+TZA.precip.raster <- crop(precip.raster, TZA.map)
+
+# 3. selecting the correct years and months for the long rainy season in Tanzania, 2008 and 2010
+
+x <- slot(slot(TZA.precip.raster, "data"), "values")
+dimnames(x)
+
+mar.y1 <- raster(TZA.precip.raster, layer = 87)
+apr.y1 <- raster(TZA.precip.raster, layer = 88)
+may.y1 <- raster(TZA.precip.raster, layer = 89)
+
+mar.y2 <- raster(TZA.precip.raster, layer = 111)
+apr.y2 <- raster(TZA.precip.raster, layer = 112)
+may.y2 <- raster(TZA.precip.raster, layer = 113)
+
+precip.raster.y1 <- stack(mar.y1, apr.y1, may.y1)
+precip.raster.y2 <- stack(mar.y2, apr.y2, may.y2)
+
+nlayers(precip.raster.y1); nlayers(precip.raster.y2)
+slot(precip.raster.y1, "layers"); slot(precip.raster.y2, "layers")
+names(precip.raster.y1); names(precip.raster.y1)
+
+# read in the geo data for 2008 and 2010
+hhid.lon.lat.y1 <- read.csv("./Analysis/Cleaned_data/hhid_lon_lat_y1.csv")
+hhid.lon.lat.y2 <- read.csv("./Analysis/Cleaned_data/hhid_lon_lat_y2.csv")
+
+coord.y1 <- select(hhid.lon.lat.y1, lon, lat)
+coord.y2 <- select(hhid.lon.lat.y2, lon, lat)
+
+precip.vals.y1 <- extract(precip.raster.y1, coord.y1)
+precip.vals.y2 <- extract(precip.raster.y2, coord.y2)
+
+precip.vars.y1 <- data.frame(cbind(hhid.lon.lat.y1, precip.vals.y1)) %>% 
+  rename(pre.mar.y1 = X2008.03.16,
+         pre.apr.y1 = X2008.04.16,
+         pre.may.y1 = X2008.05.16)
+precip.vars.y2 <- data.frame(cbind(hhid.lon.lat.y2, precip.vals.y2)) %>% 
+  rename(pre.mar.y2 = X2010.03.16,
+         pre.apr.y2 = X2010.04.16,
+         pre.may.y2 = X2010.05.16)
+
+head(precip.vars.y1, 20)
+head(precip.vars.y2, 20)
+
+write.csv(precip.var.y1, "./Analysis/Cleaned_data/precip_vars_y1.csv", row.names = FALSE)
+write.csv(precip.var.y2, "./Analysis/Cleaned_data/precip_vars_y2.csv", row.names = FALSE)
+
+# some plots
+plot(precip.raster.y1, 1)
+plot(TZA.map, add = TRUE)
+points(coord.y1, col = "blue")
+# try looking at a single community 
+points(35.85, -5.086, col = "red")
+# query with the click function to look at the raster precipitation at the point of interest
+click(precip.raster.y1)
+# from the map have a look at points in the region (36 - 38, -9 - -7) which should have high pre values
+sub.vals <- subset(coord.y1, lon > 36 & lon < 38 & lat > -9 & lat < -7)
+points(sub.vals, col = "red")
+click(precip.raster.y1)
 
 
-# 3. selecting the correct years and months - The long rainy season is (I think)
-#    March, April and MAy in Tanzania
-# x <- slot(slot(raster_TZA_y1, "data"), "values")[, c("X2010.03.16")] extract single collection of values
-# y <- slot(slot(raster_TZA_y2, "data"), "values")
-dimnames(x); dimnames(y)
-Y1_Mar <- raster(raster_TZA_y1, layer = 99)
-Y1_Apr <- raster(raster_TZA_y1, layer = 100)
-Y1_May <- raster(raster_TZA_y1, layer = 101)
-Y2_Mar <- raster(raster_TZA_y2, layer = 3)
-Y2_Apr <- raster(raster_TZA_y2, layer = 4)
-Y2_May <- raster(raster_TZA_y2, layer = 5)
-Y1_precip <- stack(Y1_Mar, Y1_Apr, Y1_May)
-Y2_precip <- stack(Y2_Mar, Y2_Apr, Y2_May)
-nlayers(Y1_precip); nlayers(Y2_precip)
-slot(Y1_precip, "layers"); slot(Y2_precip, "layers")
-names(Y1_precip); names(Y2_precip)
 
-# read in the geo data for year 1
-geo.vars.y1 <- read.csv("./Analysis/Cleaned_data/geo_vars_y1.csv")
 
-geo.var.mat <- select(geo.vars.y1, lon, lat)
-vals <- extract(Y1_precip, geo.var.mat)
-precip.vals <- data.frame(cbind(geo.var.mat, vals))
 
-# join back into geo variable
-geo.vars.y1 <- inner_join(geo.vars.y1, unique(precip.vals)) %>% rename(precip1 = X2009.03.16,
-                                                                 precip2 = X2009.04.16,
-                                                                 precip3 = X2009.05.16)
-head(select(geo.vars.y1, hhid, lon, lat, precip1, precip2:precip3), 20)
 
-# read in the geo data for year 2
-geo.vars.y2 <- read.csv("./Analysis/Cleaned_data/geo_vars_y2.csv")
 
-geo.var.mat <- select(geo.vars.y2, lon, lat)
-vals <- extract(Y2_precip, geo.var.mat)
-precip.vals <- data.frame(cbind(geo.var.mat, vals))
 
-# join back into geo variable
-geo.vars.y2 <- inner_join(geo.vars.y2, unique(precip.vals)) %>% rename(precip1 = X2011.03.16,
-                                                                    precip2 = X2011.04.16,
-                                                                    precip3 = X2011.05.16)
-head(select(geo.vars.y2, y2_hhid, lon, lat, precip1, precip2:precip3), 20)
+
+
+
+

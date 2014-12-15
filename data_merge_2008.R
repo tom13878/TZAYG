846f20d6c4ac892b601d1b271a297123435c7c02
tom13878 <tomas.morley@wur.nl>
2014-12-15 10:58:24 +0100

    Started work on code file to merge all year 1 data into a single database

--- data_merge_2008.R
+++ data_merge_2008.R
@@ -13,15 +13,20 @@ options("stringsAsFactors" = FALSE)
 options(digits = 4)
 
 # 3. read in data
-# geo variables
-# hh variables
-# plot indicator variables
-# fertilizer variables
-# output variables
-# 
-# 
-
+geo.vars <- read.csv("./Analysis/Cleaned_data/geo_vars_y1.csv")
+hh.total <- read.csv("./Analysis/Cleaned_data/hh_total_y1.csv")
+plot.IO <- read.csv("./Analysis/Cleaned_data/plot_IO_Y1.csv")
+plot.vars <- read.csv("./Analysis/Cleaned_data/plot_vars_y1.csv")
+fert.vars.winsor <- read.csv("./Analysis/Cleaned_data/fert_vars_winsor_y1.csv")
+output.maize <- read.csv("./Analysis/Cleaned_Data/output_maize_y1.csv")
+ 
 # 4. merge data into a single file
+database.2008 <- left_join(geo.vars, hh.total)
+database.2008 <- inner_join(database.2008, plot.vars)
+# database.2008 <- inner_join(database.2008, plot.IO) do something with zaocode
+database.2008 <- left_join(database.2008, fert.vars.winsor)
+database.2008 <- left_join(database.2008, output.maize)
+
 
 # 5. save as a final database
 

9a121356ee38885f1ba383b37f91bacde8be0fb1
tom13878 <tomas.morley@wur.nl>
2014-12-17 17:09:15 +0100

    Test script to see if it is possible to link up households which don't have spatial data with spatial data

---
+++ missing_hh.R
@@ -0,0 +1,3 @@
+# accounting for households that are present in some questionnaires but not in others
+geo.vars <- read.dta("./Data/Tanzania/2008_09/Stata/TZNPS1_consdta/HH.Geovariables_Y1.dta",
+                     convert.factors = TRUE)

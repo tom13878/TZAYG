9dfa8d2a1d4fae5f00e2f96f269987b3bc86d260
tom13878 <tomas.morley@wur.nl>
2014-12-18 17:24:21 +0100

    Some fixes to the rain stress script.

--- missing_hh.R
+++
@@ -1,3 +0,0 @@
-# accounting for households that are present in some questionnaires but not in others
-geo.vars <- read.dta("./Data/Tanzania/2008_09/Stata/TZNPS1_consdta/HH.Geovariables_Y1.dta",
-                     convert.factors = TRUE)

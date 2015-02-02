83867db5d8d9fa2179fd8a9d48ecfe053f7c5ace
tom13878 <tomas.morley@wur.nl>
2015-02-02 10:11:48 +0100

    Added some new functions to the winsor file

--- winsor.R
+++ winsor.R
@@ -1,30 +1,71 @@
-# created by: Tom Morley
-# Date: 22-12-2014
+#' Two functions for winsoring data. The first simply takes the values and
+#' removes the upper levels with the 0.95 quantile value. This could be extended 
+#' to deal with small values too. The second function is more complicated
+#' check out http://www.r-bloggers.com/winsorization/
 
 
-winsor <- function(df, var, x) {
+winsor1 <- function(df, var, fraction = 0.05) {
+        if (length(fraction) != 1 || fraction < 0 || fraction > 0.1){
+                stop('bad choice of fraction!!')
+        } 
         
-        ord <- order(df[, var])
-        C <- ord[x*length(ord)]
+        lim <- quantile(df[, var], probs = 1-fraction, na.rm = TRUE)
+        df[, var][df[, var] > lim] <- lim 
+        df
         
-        for (i in 1:length(df[, var])) {
-                
-                if (is.na(df[, var][i])){
-                        next
-                } else {
-                        if (df[, var][i] < df[, var][C]){
-                                next
-                        } else {
-                                df[, var][i] <- df[, var][C]
-                        } 
-                }
+        }
+        
+
+winsor2 <- function(df, var, multiple = 3){
+        if(length(multiple) != 1 || multiple <= 0){
+                stop('bad choice of multiple')
+        }
+        med <- median(df[, var], na.rm = TRUE)
+        df[, var] <- df[, var] - med
+        # note scale factor is 1.4826 for normal data assumption
+        sc <- mad(df[, var], center = 0, na.rm = TRUE)* multiple
+        print(sc)
+        df[, var][df[, var] > sc] <- sc
+        df[, var] <- df[, var] + med
+        df
+}
+
+winsor3 <- function(df, cols, multiple = 3){
+        if(length(multiple) != 1 || multiple <= 0){
+                stop('bad choice of multiple')
+        }
+        
+        for(i in cols){
+                print(i)
+                med <- median(df[, i], na.rm = TRUE)
+                df[, i] <- df[, i] - med
+                # note scale factor is 1.4826 for normal data assumption
+                sc <- mad(df[, i], center = 0, na.rm = TRUE)* multiple
+                print(sc)
+                df[, i][df[, i] > sc] <- sc
+                df[, i] <- df[, i] + med
         }
         df
-}        
+}
 
+trim1 <- function(df, col){
+        q <- quantile(df[, col], probs = c(0.01, 0.99), na.rm = TRUE)
+        ll <- q[1]
+        ul <- q[2]
+        df[,col][df[,col] < ll | df[,col] > ul] <- NA
+        df
+}
 
-# examples
-# df <- data.frame(x = sample(1:10000, 100), y = sample(1:10000, 100))
-# w1 <- winsor(df = df, "x", 0.95)
-# w2 <- winsor(df = df, x = 0.95, var = "y")
-
+trim2 <- function(df, cols){
+        for(i in cols){
+                q <- quantile(df[, i], probs = c(0.01, 0.99), na.rm = TRUE)
+                ll <- q[1]; print(ll)
+                ul <- q[2]; print(ul)
+                df[,i][df[,i] < ll | df[,i] > ul] <- NA
+        }
+        df
+}
+#' example   
+# df <- data.frame(x = rnorm(10), y = sample(1:100, 10, replace = TRUE))
+#' winsor3(df, c('x', 'y'), 3)
+#' winsor1(df, 'y', 0.05)

a4370d9ac9930e3108d5ad5b6b57a0c00f1d656c
tomas morley <tomas.morley@wur.nl>
2015-02-17 15:15:27 +0100

    deleted some unimportant files

--- Y3_data.R
+++
@@ -1,7 +0,0 @@
-library(foreign)
-
-path <- 'W:/LEI/Internationaal Beleid  (IB)/Projecten/2285000066 Africa Maize Yield Gap/SurveyData/Tanzania/2012/Data'
-setwd(path)
-
-AGSEC2A <- read.spss('./AG_SEC_2A.SAV', to.data.frame = TRUE)
-?read.dta

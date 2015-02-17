e93bbd18db9a3873cc385a1f671f87e1c9d4fc81
tomas morley <tomas.morley@wur.nl>
2015-02-17 15:09:03 +0100

    coming soon: anova analysis

--- analysis/anova_analysis.R
+++ analysis/anova_analysis.R
@@ -1,3 +1,10 @@
-""" ANOVA analysis of household and plot characteristics. Comparison made 
-between farmers who use fertilizer and those who do not. 
-"""
+# ANOVA analysis of household and plot characteristics. Comparison made 
+# between farmers who use fertilizer and those who do not. Focus on 2010
+# initially. Look seperately at variables that will be tested using the plot
+# level and the household level variables
+
+setwd('M:/TZAYG')
+
+d0 <- read.csv('M:/cleaned_data/2010/database_2010.csv')
+
+# number of observations of each group

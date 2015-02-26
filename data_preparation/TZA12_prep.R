d8324fcecd03af843924be58a2b698bb08bdd0a8
tomas morley <tomas.morley@wur.nl>
2015-02-26 10:04:13 +0100

    Created new script to prepare data for third wave of panel (2012)

---
+++ data_preparation/TZA12_prep.R
@@ -0,0 +1,6 @@
+# -------------------------------------
+#' preparation file for third wave (2012)
+#' of Tanzania data. The output of the file
+#' is a collection of databases that will be
+#' combined in the final analysis
+# -------------------------------------
